adf13cac8ea800a7e887c969da82531b6283733c
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-11-09 20:56:38 +0800

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,3 @@
+if (!(window.location.href.split('?')[0].endsWith('/') || window.location.href.split('?')[0].endsWith('.html'))) {
+    window.location.href = window.location.href.split('?')[0] + "/" +window.location.search.substring()
+}
